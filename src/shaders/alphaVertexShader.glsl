0f44048c30b7f0aaf1778cebe84b593c936ec8c6
FulgurIgor <i.s.ger@ya.ru>
2020-04-09 15:43:55 +0300

    Break shader

--- src/shaders/alphaVertexShader.glsl
+++ src/shaders/alphaVertexShader.glsl
@@ -2,9 +2,13 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in float alpha;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 out float ourAlpha;
 void main()
 {
-    gl_Position = vec4(position, 1.0f);
+    gl_Position = projection * view * model * vec4(position, 1.0f);
     ourAlpha = alpha;
 }
